308dce931717030da599ae218bed754457bfc318
DrByte <drbyte@zen-cart.com>
2016-04-11 20:54:45 -0400

    Optimizations for building the default PHP 7.0 box

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -28,6 +28,11 @@ apt-get install -y software-properties-common curl
 
 #Ubuntu Precise 12.04 includes PHP 5.3.10 by default
 #Ubuntu Trusty 14.04 installs PHP 5.5.9 by default
+
+apt-add-repository ppa:ondrej/php -y
+
+#PHP 7.0
+apt-add-repository ppa:ondrej/php70 -y
 #PHP 5.6
 #apt-add-repository ppa:ondrej/php5-5.6 -y
 #PHP 5.5
@@ -39,6 +44,11 @@ apt-get install -y software-properties-common curl
 #apt-add-repository ppa:ondrej/mysql-5.6 -y
 #apt-add-repository ppa:ondrej/mysql-5.7 -y
 #apt-add-repository ppa:ondrej/mariadb-5.5 -y
+
+# gpg: key 5072E1F5: public key "MySQL Release Engineering <mysql-build@oss.oracle.com>" imported
+apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 5072E1F5
+sh -c 'echo "deb http://repo.mysql.com/apt/ubuntu/ trusty mysql-5.7" >> /etc/apt/sources.list.d/mysql.list'
+
 
 # Update Package Lists
 
@@ -59,21 +69,17 @@ ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 
 cp /vagrant/aliases /home/vagrant/.bash_aliases
 
-# Install PHP Stuff
+# Install PHP 7
+apt-get install -y --force-yes php7.0-cli php7.0-dev \
+php-sqlite3 php-gd php-apcu \
+php-curl php7.0-mcrypt \
+php-imap php-mysql php-memcached php7.0-readline php-xdebug \
+php-mbstring php-xml php7.0-zip php7.0-intl php7.0-bcmath
 
-apt-get install -y php5-cli php5-dev php-pear \
-php5-mysqlnd php5-sqlite \
-php5-json php5-curl php5-gd \
-php5-gmp php5-imap php5-mcrypt php5-xdebug \
-php5-memcached \
-php5-xsl
-# do this separately to accommodate older PHP versions
-apt-get install -y php5-apcu
-
-sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/cli/php.ini
-sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/cli/php.ini
-sudo sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php5/cli/php.ini
-sudo sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php5/cli/php.ini
+sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.0/cli/php.ini
+sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.0/cli/php.ini
+sudo sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.0/cli/php.ini
+sudo sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.0/cli/php.ini
 
 
 # Install Composer
@@ -117,7 +123,7 @@ EOF
 
 
 # sync error logs
-sudo sed -i "s/;error_log = php_errors.log/error_log = \/home\/vagrant\/habitat\/php_errors.log/" /etc/php5/cli/php.ini
+#sudo sed -i "s/;error_log = php_errors.log/error_log = \/home\/vagrant\/habitat\/php_errors.log/" /etc/php7.0/cli/php.ini
 
 # Add Vagrant User To WWW-Data
 
@@ -126,7 +132,7 @@ id vagrant
 groups vagrant
 
 # Install Apache
-apt-get install -y apache2 libapache2-mod-php5 apache2-utils
+apt-get install -y apache2 libapache2-mod-php7.0 apache2-utils
 sed -i "s/export APACHE_RUN_USER=www-data/export APACHE_RUN_USER=vagrant/" /etc/apache2/envvars
 sed -i "s/export APACHE_RUN_GROUP=www-data/export APACHE_RUN_GROUP=vagrant/" /etc/apache2/envvars
 sed -i "s/export APACHE_LOG_DIR=\/var\/log\/apache2$SUFFIX/export APACHE_LOG_DIR=\/home\/vagrant\/habitat\/$SUFFIX/" /etc/apache2/envvars
@@ -139,22 +145,24 @@ a2enmod ssl rewrite
 service apache2 restart
 
 # Set apache2-php settings
-sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
-sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
-sudo sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php5/apache2/php.ini
-sudo sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php5/apache2/php.ini
-sudo sed -i "s/post_max_size = .*/post_max_size = 512M/" /etc/php5/apache2/php.ini
-sudo sed -i "s/upload_max_filesize = .*/upload_max_filesize = 512M/" /etc/php5/apache2/php.ini
-sudo sed -i "s/html_errors = .*/html_errors = Off/" /etc/php5/apache2/php.ini
-sudo sed -i "s/;error_log = php_errors.log/error_log = \/home\/vagrant\/habitat\/php_errors.log/" /etc/php5/apache2/php.ini
+sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.0/apache2/php.ini
+sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.0/apache2/php.ini
+sudo sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.0/apache2/php.ini
+sudo sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.0/apache2/php.ini
+sudo sed -i "s/post_max_size = .*/post_max_size = 512M/" /etc/php/7.0/apache2/php.ini
+sudo sed -i "s/upload_max_filesize = .*/upload_max_filesize = 512M/" /etc/php/7.0/apache2/php.ini
+sudo sed -i "s/html_errors = .*/html_errors = Off/" /etc/php/7.0/apache2/php.ini
+sudo sed -i "s/;error_log = php_errors.log/error_log = \/home\/vagrant\/habitat\/php_errors.log/" /etc/php/7.0/apache2/php.ini
 
 # Install SQLite
 
 apt-get install -y sqlite3 libsqlite3-dev
 
 # Install MySQL
-debconf-set-selections <<< "mysql-server mysql-server/root_password password zencart"
-debconf-set-selections <<< "mysql-server mysql-server/root_password_again password zencart"
+
+debconf-set-selections <<< "mysql-community-server mysql-community-server/data-dir select ''"
+debconf-set-selections <<< "mysql-community-server mysql-community-server/root-pass password zencart"
+debconf-set-selections <<< "mysql-community-server mysql-community-server/re-root-pass password zencart"
 apt-get install -y mysql-server
 
 # Add Timezone Support To MySQL
@@ -202,7 +210,8 @@ debconf-set-selections <<< "dbconfig-common dbconfig-common/app-password-confirm
 debconf-set-selections <<< "dbconfig-common dbconfig-common/app-password-confirm password zencart"
 debconf-set-selections <<< "dbconfig-common dbconfig-common/password-confirm password zencart"
  # Handy for debugging. clear answers phpmyadmin: echo PURGE | debconf-communicate phpmyadmin
-apt-get -y install phpmyadmin
+apt-add-repository ppa:nijel/phpmyadmin -y
+apt-get -y install phpmyadmin libapache2-mod-php7.0
 sudo sed -i "s/$cfg\['UploadDir'] = .*;/$cfg['UploadDir'] = '\/var\/lib\/phpmyadmin\/tmp';/" /etc/phpmyadmin/config.inc.php
 sudo sed -i "s/$cfg\['SaveDir'] = .*;/$cfg['SaveDir'] = '\/var\/lib\/phpmyadmin\/tmp';/" /etc/phpmyadmin/config.inc.php
 #ln -s /usr/share/phpmyadmin /usr/share/nginx/html
